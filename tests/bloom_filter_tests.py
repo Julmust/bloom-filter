82fd5631868fbc6e472223e0457fe51f4b3fda36
Jesper Bergeskans <jesper.bergeskans@gmail.com>
2024-04-29 20:41:55 +0200

    Adding tests of object

--- tests/bloom_filter_tests.py
+++ tests/bloom_filter_tests.py
@@ -1,6 +1,27 @@
-from bloom_filter import hash
+from bloom_filter import BloomFilter
 
 
 def test_hashing():
-    res = hash('Hello, world!')
+    bf = BloomFilter()
+    res = bf.hash('Hello, world!')
+
     assert res == [643, 403, 247]
+
+def test_insert():
+    bf = BloomFilter()
+    bf.insert('Hello, world!')
+
+    for v in [643, 403, 247]:
+        assert bf.arr[v] == 1
+
+def test_is_present_true():
+    bf = BloomFilter()
+    bf.insert('Hello, world!')
+
+    assert bf.is_present(bf.hash('Hello, world!'))
+
+def test_is_present_false():
+    bf = BloomFilter()
+    bf.insert('Hello, world!')
+
+    assert not bf.is_present(bf.hash('Hello, world'))
