17f36f70570dbd5819565ff555e93b4b2338bd8b
Jesper Bergeskans <jesper.bergeskans@gmail.com>
2024-04-29 20:48:20 +0200

    Adding docstrings

--- bloom_filter/bloom_filter.py
+++ bloom_filter/bloom_filter.py
@@ -7,9 +7,15 @@ class BloomFilter:
         self.arr = [0] * 1000
 
     def hash(self, val: str) -> list:
+        '''
+        Consistently hash the incoming values with a seed
+        '''
         return [mmh3.hash(val, seed) % 1000 for seed in self.seeds]
 
     def insert(self, val):
+        '''
+        Insert value if it is not present.
+        '''
         hp = self.hash(val)
 
         # If the value _might_ be present, we don't have to edit the array
@@ -19,6 +25,10 @@ class BloomFilter:
 
 
     def is_present(self, val) -> bool:
+        '''
+        Check if a value might be present. With Bloom filters false positives
+        are possible but not false negatives.
+        '''
         hp = self.hash(val)
         total = sum([self.arr[p] for p in hp])
 
